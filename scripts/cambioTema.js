06771c86486aae5143bd77d414fb2e3eb75f8be0
juannpg <juan.pasgut.workspace@gmail.com>
2024-05-27 21:00:47 +0200

    script done

--- scripts/cambioTema.js
+++ scripts/cambioTema.js
@@ -2,19 +2,19 @@ const botonClaro = document.getElementById("temaClaro");
 const botonOscuro = document.getElementById("temaOscuro");
 const calculadora = document.getElementById("calculadora");
 
-botonClaro.addEventListener("click", modoClaro());
-botonOscuro.addEventListener("click", modoOscuro());
+botonClaro.addEventListener("click", modoClaro);
+botonOscuro.addEventListener("click", modoOscuro);
 
 function modoClaro() {
   body.style.backgroundColor = "rgb(253, 253, 242)";
   calculadora.style.backgroundColor = "#fff3e8";
   button.style.backgroundColor = "#f7e5c5";
-  button.style.color = "rgb(161, 161, 161";
+  button.style.color = "rgb(161, 161, 161)";
 }
 
 function modoOscuro() {
-  body.style ="background-color: rgba(18,18,18,255);";
-  calculadora.style ="background-color: #2e2e2e;";
-  button.style = "background-color :#535353;";
-  button.style = "color: rgb(161, 161, 161);";
+  body.style.backgroundColor ="rgba(18,18,18,255)";
+  calculadora.style.backgroundColor ="#2e2e2e";
+  button.style.backgroundColor = "#535353";
+  button.style.color = "rgb(161, 161, 161)";
 }
