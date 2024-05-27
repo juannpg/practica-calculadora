6a1b33a7933ccd9db5bcb9111ca13d9b11641ea4
juannpg <juan.pasgut.workspace@gmail.com>
2024-05-27 20:56:52 +0200

    arreglo errores js

--- scripts/cambioTema.js
+++ scripts/cambioTema.js
@@ -2,14 +2,14 @@ const botonClaro = document.getElementById("temaClaro");
 const botonOscuro = document.getElementById("temaOscuro");
 const calculadora = document.getElementById("calculadora");
 
-botonClaro.addEventListener("click", modoClaro());
-botonOscuro.addEventListener("click", modoOscuro());
+botonClaro.addEventListener("click", modoClaro);
+botonOscuro.addEventListener("click", modoOscuro);
 
 function modoClaro() {
   body.style.backgroundColor = "rgb(253, 253, 242)";
   calculadora.style.backgroundColor = "#fff3e8";
   button.style.backgroundColor = "#f7e5c5";
-  button.style.color = "rgb(161, 161, 161";
+  button.style.color = "rgb(161, 161, 161)";
 }
 
 function modoOscuro() {
