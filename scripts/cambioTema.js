0787b8c5081fc37a9eb2f7212c15a4ff0318a1d4
juannpg <juan.pasgut.workspace@gmail.com>
2024-05-27 23:41:22 +0200

    arreglada la funcionalidad cambiar de tema, eliminado el hover de los botones

--- scripts/cambioTema.js
+++ scripts/cambioTema.js
@@ -1,6 +1,9 @@
 const botonClaro = document.getElementById("temaClaro");
 const botonOscuro = document.getElementById("temaOscuro");
+
 const calculadora = document.getElementById("calculadora");
+const body = document.getElementById("body");
+const buttons = document.querySelectorAll("button");
 
 botonClaro.addEventListener("click", modoClaro);
 botonOscuro.addEventListener("click", modoOscuro);
@@ -8,13 +11,17 @@ botonOscuro.addEventListener("click", modoOscuro);
 function modoClaro() {
   body.style.backgroundColor = "rgb(253, 253, 242)";
   calculadora.style.backgroundColor = "#fff3e8";
-  button.style.backgroundColor = "#f7e5c5";
-  button.style.color = "rgb(161, 161, 161)";
+  buttons.forEach((button) => {
+    button.style.backgroundColor = "#f7e5c5";
+    button.style.color = "rgb(161, 161, 161)";
+  })
 }
 
 function modoOscuro() {
   body.style.backgroundColor ="rgba(18,18,18,255)";
   calculadora.style.backgroundColor ="#2e2e2e";
-  button.style.backgroundColor = "#535353";
-  button.style.color = "rgb(161, 161, 161)";
+  buttons.forEach((button) => {
+    button.style.backgroundColor = "#535353";
+    button.style.color = "rgb(161, 161, 161)";
+  })
 }
