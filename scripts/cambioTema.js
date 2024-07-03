84b52059d659efc68a55c86047edca59dc3c765a
juannpg <juan.pasgut.workspace@gmail.com>
2024-07-03 13:49:28 +0200

    bug fix #02 hover en los botones. cambiado el metodo de cambio de tema para que use clases y así poder hacer el hover

--- scripts/cambioTema.js
+++ scripts/cambioTema.js
@@ -10,20 +10,24 @@ toggle.addEventListener("click", cambioTema)
 function cambioTema() {
   if (i === 0) {
     //activar modo claro
-    body.style.backgroundColor = "rgb(253, 253, 242)";
-    main.style.backgroundColor = "#fff3e8";
+    body.classList.remove('oscuro');
+    body.classList.add('claro');
+    main.classList.remove('oscuro');
+    main.classList.add('claro');
     buttons.forEach((button) => {
-      button.style.backgroundColor = "#f7e5c5";
-      button.style.color = "rgb(161, 161, 161)";
+      button.classList.remove('oscuro');
+      button.classList.add('claro');
     })
     i++;
   } else if (i === 1){
     //activar modo oscuro
-    body.style.backgroundColor ="rgba(18,18,18,255)";
-    main.style.backgroundColor ="#2e2e2e";
+    body.classList.remove('claro');
+    body.classList.add('oscuro');
+    main.classList.remove('claro');
+    main.classList.add('oscuro');
     buttons.forEach((button) => {
-      button.style.backgroundColor = "#535353";
-      button.style.color = "rgb(161, 161, 161)";
+      button.classList.remove('claro');
+      button.classList.add('oscuro');
     })
     i--;
   }
